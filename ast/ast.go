fee66efabf5a4055a512ddff640a99517c13198d
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-07-31 22:32:12 +0100

    added more types

--- ast/ast.go
+++ ast/ast.go
@@ -41,3 +41,16 @@ func (ls *LetStatement) statementNode() {
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
